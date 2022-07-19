e6e3c3b82c3cef047c3f71c822064ce7d275570c
marvin <j123211@qq.com>
2022-07-19 13:08:26 +0800

    Update feast.go

--- feast/feast.go
+++ feast/feast.go
@@ -6,13 +6,5 @@ import (
 )
 
 func main(){
-	envs := os.Environ()
-	for _, e := range envs {
-		parts := strings.SplitN(e, "=", 2)
-		if len(parts) != 2 {
-			continue
-		}else{
-			println(string(parts[0]),string(parts[1]))
-		}
-	}
+	println(os.LookupEnv("FEAST_TOKEN"))
 }
